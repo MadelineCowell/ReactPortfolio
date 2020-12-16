4486fe7ca9c4be07e363a1e85da38bf6efabd1c1
MadelineC <madeline.cowell@colorado.edu>
2020-12-15 21:58:39 -0700

    Creating card component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -10,14 +10,47 @@ function Cards() {
             <div className="cards__container">
                 <div className="wrapper">
                     <ul className="cards__items">
-                        <CardItem 
-                        src="../assets/images/CVLogo.png"
-                        text="View my resume"
-                        label='Resume'
-                        path='/contact'/>
+                        <CardItem
+                            src="\images\TacoPlaylist.PNG"
+                            text="Taco Playlist Generator"
+                            label='Project'
+                            path='/project'
+                        />
+                        <CardItem
+                        src='images/JS-Quiz.PNG'
+                        text='View my interactive quiz, utilizing Javascript'
+                        label='Project'
+                        path='/project'
+                        />
+                        <CardItem
+                        src='\images\PasswordGenerator.PNG'
+                        text='View my interactive quiz, utilizing Javascript'
+                        label='Project'
+                        path='/projects'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                            src="\images\GitHub.png"
+                            text="GitHub"
+                            label='Resource'
+                            path='/connect'
+                        />
+                        <CardItem
+                        src='images/LinkedIn.png'
+                        text='LinkedIn'
+                        label='Resource'
+                        path='/connect'
+                        />
+                        <CardItem
+                        src='\images\Resume.png'
+                        text='Resume'
+                        label='Resource'
+                        path='/connect'
+                        />
                     </ul>
                 </div>
-            </div>
+            </div> 
         </div>
     )
 }
