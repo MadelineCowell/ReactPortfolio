0b79822b5bb2c08e9f01bde0aecece131ff12da8
MadelineC <madeline.cowell@colorado.edu>
2020-12-16 18:54:40 -0700

    Creating pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Navbar from '../src/components/navbar/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home'
+import Main from './components/pages/Main'
+import About from './components/pages/About'
+import Projects from './components/pages/Projects'
+import Portfolio from './components/pages/Portfolio'
+import Resume from './components/pages/Resume'
+import Contact from './components/pages/Contact'
 
 
 function App() {
@@ -14,7 +20,13 @@ function App() {
           <Navbar />
           <Switch>
             <Route path='/' exact component={Home} />
-          </Switch>
+            <Route path='/main' exact component={Main} />
+            <React path='/about' component={About} />
+            <React path='/contact' component={Contact} />
+            <Route path='/projects' exact component={Projects} />
+            <Route path='/portfolio' exact component={Portfolio} />
+            <Route path='/resume' exact component={Resume} />
+          </Switch> 
         </div>
       </Router>
     </>
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css'
+import Banner from '../Banner/Banner'
+import Cards from '../Cards/Cards'
+
+function Resume() {
+    return (
+        <>
+        <Banner />
+        </>
+    )
+}
+
+
+export default Resume
